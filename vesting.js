1f05e5e82ea78b368b85ebefcb0b0beb744a8f3a
Martin Caslavsky <mcaslavsky@gmail.com>
2013-05-04 21:19:06 +0200

    initial version

---
+++ vesting.js
@@ -0,0 +1,36 @@
+angular.module("vesting", ['ui.date'])
+.controller("VestingCtrl", function ($scope) {
+	$scope.effectiveDate = new Date();
+	$scope.startDate = new Date();
+	$scope.startDate.setFullYear($scope.startDate.getFullYear() - 1);
+	$scope.vestingLength = 4;
+	$scope.optionsNumber = 1000;
+
+	$scope.fullMonths = function() { 
+		return monthsDiff($scope.startDate, $scope.effectiveDate);
+	};
+
+	$scope.totalMonths = function() { 
+		return monthsDiff($scope.startDate, $scope.vestingEnds());
+	};
+
+	$scope.vestingEnds = function() {
+		var end = new Date($scope.startDate.getTime());
+		end.setFullYear($scope.startDate.getFullYear() + $scope.vestingLength);
+		return end;
+	};
+
+	$scope.vested = function() {
+		return Math.round($scope.optionsNumber / $scope.totalMonths() * $scope.fullMonths());
+	}
+})
+;
+
+
+function monthsDiff(start, end) {
+	var months;
+	months = (end.getFullYear() - start.getFullYear()) * 12;
+	months -= start.getMonth() + 1;
+	months += end.getMonth();
+	return months <= 0 ? 0 : months;
+}
