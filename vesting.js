7e3c2b07ff563765af2d7b6d40317e8fd8c4287b
Martin Caslavsky <mcaslavsky@gmail.com>
2013-06-18 10:29:32 +0200

    chg: extracted vesting dto

--- vesting.js
+++ vesting.js
@@ -1,40 +1,48 @@
-angular.module("vesting", ['ui.date','ui.bootstrap'])
-.controller("VestingCtrl", function ($scope) {
-	$scope.effectiveDate = new Date();
-	$scope.startDate = new Date();
-	$scope.startDate.setFullYear($scope.startDate.getFullYear() - 1);
-	$scope.vestingLength = 4;
-	$scope.optionsNumber = 1000;
+function Vesting(date, vestingLengthF, optionsNumberF) {
+	this.effectiveDate = new Date(date.getTime());
+	this.startDate = new Date(date.getTime());
+	this.startDate.setFullYear(date.getFullYear() - 1);
+	this.vestingLength = vestingLengthF;
+	this.optionsNumber = optionsNumberF;
 
-	$scope.fullMonths = function() { 
-		return monthsDiff($scope.startDate, $scope.effectiveDate);
+	this.monthsDiff = function (start, end) {
+		var months;
+		months = (end.getFullYear() - start.getFullYear()) * 12;
+		months -= start.getMonth() + 1;
+		months += end.getMonth();
+		return months <= 0 ? 0 : months;
+	}
+
+	this.fullMonths = function() {
+		return this.monthsDiff(this.startDate, this.effectiveDate);
 	};
 
-	$scope.totalMonths = function() { 
-		return monthsDiff($scope.startDate, $scope.vestingEnds());
+	this.totalMonths = function() {
+		return this.monthsDiff(this.startDate, this.vestingEnds());
 	};
 
-	$scope.vestingEnds = function() {
-		var end = new Date($scope.startDate.getTime());
-		end.setFullYear($scope.startDate.getFullYear() + $scope.vestingLength);
+	this.vestingEnds = function() {
+		var end = new Date(this.startDate.getTime());
+		end.setFullYear(this.startDate.getFullYear() + this.vestingLength);
 		return end;
 	};
 
-	$scope.vested = function() {
-		return Math.round($scope.optionsNumber / $scope.totalMonths() * $scope.fullMonths());
+	this.vested = function() {
+		var v = Math.round(this.optionsNumber / this.totalMonths() * this.fullMonths());
+console.log(v);
+		return v;
 	};
 
-	$scope.vestedPct = function() {
-		return Math.round(100 / $scope.totalMonths() * $scope.fullMonths());
+	this.vestedPct = function() {
+		return Math.round(100 / this.totalMonths() * this.fullMonths());
 	};
-})
+
+}
+
+angular.module("vesting", ['ui.date','ui.bootstrap'])
+	.controller("VestingCtrl", function ($scope) {
+		$scope.vest = new Vesting(new Date(), 4, 1000);
+	})
 ;
 
 
-function monthsDiff(start, end) {
-	var months;
-	months = (end.getFullYear() - start.getFullYear()) * 12;
-	months -= start.getMonth() + 1;
-	months += end.getMonth();
-	return months <= 0 ? 0 : months;
-}
